020db93dadf71918fb1e564e8f050f78ccaa7716
Alex <o.a.momotyuk@gmail.com>
2019-08-24 23:42:01 -0500

    initial files

---
+++ js/blog.js
@@ -0,0 +1,10 @@
+/*
+
+Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
+Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+Obtain a reference the element with a class of dashed and remove it.
+Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+
+*/
+
+
